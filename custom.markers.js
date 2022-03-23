ee82985f72ea45d0dfc7d2de48aa904fd7e1d51b
counterfitninja <dave@chaoticblue.co.uk>
2022-03-23 13:35:49 +0000

    Update Maps

--- custom.markers.js
+++ custom.markers.js
@@ -1,4 +1,6 @@
  UnminedCustomMarkers = { isEnabled: true, markers: [ 
+{ x: -216, z: -355, image: "../markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "CNinja's Base", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
+
 
 ]
 }
