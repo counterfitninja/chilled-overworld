dfc62ca290469d41d52ccbd1f46977af24c68104
counterfitninja <dave@chaoticblue.co.uk>
2022-03-23 15:26:42 +0000

    Update Maps

--- custom.markers.js
+++ custom.markers.js
@@ -1,9 +1,7 @@
  UnminedCustomMarkers = { isEnabled: true, markers: [ 
-{ x: -216, z: -355, image: "../markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "CNinja's Base", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
+{ x: -216, z: -355, image: "./markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "CNinja's Base", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
 
-{ x: -428, z: 135, image: "../markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "Village With Villagers On A Mountain", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
-
-{ x: -428, z: 135, image: "../markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "Village With Villagers On A Mountain", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
+{ x: -428, z: 135, image: "./markers/map-marker-20.png", imageAnchor: [0.5, 1], imageScale: 0.5, text: "Village With Villagers On A Mountain", textColor: "#ffffff", offsetX: 0, offsetY: 8, font: "bold 12px Verdana" },
 
 
 ]
